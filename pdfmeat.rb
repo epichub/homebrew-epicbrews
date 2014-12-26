8b37a4b5bf4f7d06fcadcaa942ca6cb20854da13
Bjørn Magnus Mathisen <bjornmagnus.mathisen@sintef.no>
2014-12-26 22:05:28 +0100

    fixed pdfmeat formulae

--- pdfmeat.rb
+++ pdfmeat.rb
@@ -1,15 +1,17 @@
 require 'formula'
 
-class PDFmeat < Formula
+class Pdfmeat < Formula
   homepage 'https://github.com/mankoff/pdfmeat'
   url 'https://github.com/mankoff/pdfmeat.git'
   sha1 '00437eed6bf5d3fede441aaa7e32a863fd2a27be'
   #  sha1 'd64282e744424ec7c5f92b66e78e5a6fded90ff9'
 
+  version 'head'
+  
 #  depends_on :automake
 #  def patches
 #  end
   def install
-    system "cp" "pdfmeat.py" "#{HOMEBREW_PREFIX}/bin"
+    system "cp", "pdfmeat.py", "#{HOMEBREW_PREFIX}/bin"
   end
 end
